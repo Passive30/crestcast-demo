1f6bc1a99ceee0d8ed6132f7a98622f285c9651b
Passive30 <alan@crestcastai.com>
2025-07-18 12:38:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -565,7 +565,7 @@ if st.checkbox("Show 1yr, 5yr, 10yr, Since Inception Statistics"):
 
 if st.checkbox("Show Rolling 5-Year Alpha Summary and Distribution"):
 
-    rolling_window = 120  # 5 years
+    rolling_window = 60  # 5 years
     alpha_values = []
     alpha_dates = []
 
@@ -610,7 +610,7 @@ if st.checkbox("Show Rolling 5-Year Alpha Summary and Distribution"):
 
 if st.checkbox("Show Rolling 5-Year Sharpe Comparison"):
     # Sharpe stats and chart
-    rolling_window = 36  # 5 years
+    rolling_window = 120  # 5 years
     crest_sharpes = []
     bench_sharpes = []
     dates = []
